e6096c86d4e32627bd48f8be7de93838f7f9f01e
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-12-24 00:39:28 +0200

    auth middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -5,22 +5,39 @@ const authUser = async (req, res, next) => {
     try{
         const authHeader = req.headers.authorization;
         if (!authHeader){
-            return res.status(400).json({msg: 'Authorization header is required'});
+            return res.status(400).json({
+                error: 'Authorization header is required'
+            });
         }
         const parts = authHeader.split(' ');
         if (parts.length !== 2 || parts[0] !== 'Bearer'){
-            return res.status(401).json({ error: 'Invalid authorization header format. Expected "Bearer <token>"' });
+            return res.status(401).json({
+                error: 'Invalid authorization header format. Expected "Bearer <token>"'
+            });
         }
         const authToken = parts[1];
-        const validToken = verifyAccessToken(authToken);
-        const user = await User.findOne({_id: validToken.userId });
+        const decoded = verifyAccessToken(authToken);
+        const user = await User.findOne({_id: decoded.userId });
         if (!user){
-            return res.status(404).json({error: 'User not Found' });
+            return res.status(401).json({
+                error: 'Invalid authentication credentials'
+            });
         }
         req.user = user;
         next();
     }catch(error){
-        return res.status(500).json({error: error.message});
+        if (error.name === 'JsonWebTokenError'){
+            return res.status(401).json({
+                error: 'Invalid token'
+            });
+        }if (error.name === 'TokenExpiredError'){
+           return res.status(401).json({
+            error: 'Token has expired'
+           })
+        }
+        return res.status(500).json({
+            error: 'Internal server error'
+        })
     }
 }
 export default  authUser;
